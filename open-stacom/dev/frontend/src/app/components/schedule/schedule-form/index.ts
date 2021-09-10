e6f6f8a0e5a8c2aafaedf5a6dd8c0b3c7260be0a
Lucas Fonseca dos Santos <fonsecalucassantos@gmail.com>
2021-09-10 14:42:42 -0300

    Generating schedule entity component of AngularJS

---
+++ open-stacom/dev/frontend/src/app/components/schedule/schedule-form/index.ts
@@ -0,0 +1,1 @@
+export * from './schedule-form.component';
---
+++ open-stacom/dev/frontend/src/app/components/schedule/schedule-table/schedule-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-table',
+  templateUrl: './schedule-table.component.html',
+  styleUrls: ['./schedule-table.component.scss']
+})
+export class ScheduleTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
