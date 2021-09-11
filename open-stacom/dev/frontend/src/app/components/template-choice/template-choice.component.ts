310ddd6659a093472ef5a3ef06d0f7d487510298
Lucas Fonseca dos Santos <fonsecalucassantos@gmail.com>
2021-09-10 22:07:20 -0300

    Create template choice component

---
+++ open-stacom/dev/frontend/src/app/components/template-choice/template-choice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-choice',
+  templateUrl: './template-choice.component.html',
+  styleUrls: ['./template-choice.component.scss']
+})
+export class TemplateChoiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
