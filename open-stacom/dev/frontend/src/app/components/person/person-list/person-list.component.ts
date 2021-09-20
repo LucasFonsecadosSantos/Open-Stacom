99ebaed2e53c44904758a8e06557e93a6ee4a4ba
Lucas Fonseca dos Santos <fonsecalucassantos@gmail.com>
2021-09-19 21:35:47 -0300

    Template find use case service implemented

--- open-stacom/dev/frontend/src/app/components/person/person-list/person-list.component.ts
+++ open-stacom/dev/frontend/src/app/components/person/person-list/person-list.component.ts
@@ -1,6 +1,16 @@
+import { TemplateFindService } from './../../../services/templates/template-find.service';
+import { EventFindService } from './../../../services/event/event-find.service';
 import { PersonFindService } from './../../../services/person/person-find.service';
 import { Component, OnInit } from '@angular/core';
-import { Person } from 'src/app/models';
+import {
+  Person,
+  Template
+} from 'src/app/models';
+import {
+  ActivatedRoute,
+  ParamMap,
+  Router
+} from '@angular/router';
 
 @Component({
   selector: 'app-person-list',
@@ -9,17 +19,57 @@ import { Person } from 'src/app/models';
 })
 export class PersonListComponent implements OnInit {
 
-  personArray: Person[];
+  template:     Template;
+  eventID:      string;
+  personArray:  Person[];
 
   constructor(
-
-    private personFindService: PersonFindService
+    private route: ActivatedRoute,
+    private router: Router,
+    private paramMap: ParamMap,
+    private eventFindService:     EventFindService,
+    private templateFindService:  TemplateFindService,
+    private personFindService:    PersonFindService,
   ) { }
 
   ngOnInit(): void {
 
-
+    this._getEventIDFromRoute();
+    this._getTemplate(this._getTemplateID());
     this._listPeople();
+
+  }
+
+  private _getTemplate(templateID: string): void {
+
+    this.templateFindService.find(templateID)
+      .subscribe(
+        response => {
+          this.template = response;
+        }
+      );
+
+  }
+
+  private _getTemplateID(): string {
+
+    let templateID: string;
+
+    this.eventFindService.find(this.eventID)
+      .subscribe(
+        response => {
+          templateID = response.templateID;
+        }
+      );
+
+    return templateID;
+  }
+
+  private  _getEventIDFromRoute(): void {
+
+    this.route.queryParamMap.subscribe(params => {
+      this.eventID = params['eventID'];
+    });
 
   }
 
--- open-stacom/dev/frontend/src/app/services/templates/template-find.service.ts
+++ open-stacom/dev/frontend/src/app/services/templates/template-find.service.ts
@@ -1,9 +1,21 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemplateFindService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  find(eventID: string): Observable<Template> {
+
+    return this.http.get<Template>(`${environment.API_URL.EVENT}${environment.API_URL.EVENT}/${eventID}`);
+
+  }
 }
