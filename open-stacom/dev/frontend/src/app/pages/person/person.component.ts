abd5be45204ac1e793270cbbb87e87b04b42c2e4
Lucas Fonseca dos Santos <fonsecalucassantos@gmail.com>
2021-09-26 21:05:57 -0300

    Fixing somethings

--- open-stacom/dev/frontend/src/app/pages/person/person.component.ts
+++ open-stacom/dev/frontend/src/app/pages/person/person.component.ts
@@ -1,8 +1,10 @@
+import { PersonListComponent } from './../../components/person/person-list/person-list.component';
 import { PersonPageService } from './person-page.service';
 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Person } from 'src/app/models';
 
 @Component({
   selector: 'app-person',
@@ -10,6 +12,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./person.component.scss']
 })
 export class PersonComponent implements OnInit {
+
+  @ViewChild(PersonListComponent)
+  private personListComponent: PersonListComponent;
+
+  private _personArray: Person[];
 
   closeResult: string;
 
@@ -28,8 +35,23 @@ export class PersonComponent implements OnInit {
         ariaLabelledBy: 'modal-basic-title',
         windowClass: 'modal-custom',
         size: 'lg',
-        centered: true
+        centered: true,
+        modalDialogClass: 'modal-dialog-custom'
 
+
+      }
+    )
+  }
+
+  exportExcel(): void {
+    this._personArray = this.personListComponent.personArray;
+
+  }
+
+  confirmDeleteAllPeople(dialog: any): void {
+    this.modalService.open(dialog,
+      {
+        size: 'md'
       }
     )
   }
