107d7dcb46bb61194f1570336efc34e77a64f214
Lucas Fonseca dos Santos <fonsecalucassantos@gmail.com>
2021-09-26 23:02:18 -0300

    Lattest updates

--- open-stacom/dev/frontend/src/app/components/person/person-info/person-info.component.ts
+++ open-stacom/dev/frontend/src/app/components/person/person-info/person-info.component.ts
@@ -1,3 +1,5 @@
+import { UrlService } from './../../../services/utils/url.service';
+import { PersonDeleteService } from './../../../services/person/person-delete.service';
 import { PersonPageService } from './../../../pages/person/person-page.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Person } from 'src/app/models';
@@ -12,16 +14,30 @@ export class PersonInfoComponent implements OnInit {
   person: Person;
 
   constructor(
-    private personPageService: PersonPageService
+    private _personPageService:   PersonPageService,
+    private _personDeleteService: PersonDeleteService,
+    private _urlService:          UrlService
   ) { }
 
   ngOnInit(): void {
 
-    this.personPageService
+    this._personPageService
+
           .getSelectedPerson()
+
           .subscribe(person => {
             this.person = person;
           });
+
+  }
+
+  public editPerson(person: Person): void {}
+
+  public deletePerson(person: Person): void {
+
+
+    this._personDeleteService.delete(person.id, this._urlService.getEventIDFromRoute());
+
   }
 
 
