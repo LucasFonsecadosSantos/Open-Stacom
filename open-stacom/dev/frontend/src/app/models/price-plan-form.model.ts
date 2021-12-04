2e57735b7e1b322b8a1a96b81e4b3eb657cde925
Lucas Fonseca dos Santos <fonsecalucassantos@gmail.com>
2021-12-04 18:57:47 -0300

    Creating price plan models

--- open-stacom/dev/frontend/src/app/models/price-plan-form.model.ts
+++ open-stacom/dev/frontend/src/app/models/price-plan-form.model.ts
@@ -1,2 +1,10 @@
+import { PricePlan } from ".";
+import { Operation } from "../enums";
+
 export class PricePlanForm {
+
+  title?: string;
+  pricePlan?: PricePlan;
+  operation?: Operation;
+
 }
--- open-stacom/dev/frontend/src/app/models/price-plan.model.ts
+++ open-stacom/dev/frontend/src/app/models/price-plan.model.ts
@@ -1,2 +1,8 @@
 export class PricePlan {
+
+  id?: string;
+  name?: string;
+  value?: string;
+  description?: string;
+
 }
